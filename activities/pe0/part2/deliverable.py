ebec5e7adab0adeebfef2f4eee765d18a9ed1265
huifenglan <huifeng1@vt.edu>
2019-05-28 14:14:33 -0400

    Update deliverable.py

--- activities/pe0/part2/deliverable.py
+++ activities/pe0/part2/deliverable.py
@@ -1,3 +1,13 @@
 #!/usr/bin/env python3
 
+a = int(input("input a number"+'\n')) #user input
 
+#condition checks
+if((a % 3 == 0) and (a % 5 == 0)):
+    print ("fizzbuzz")
+elif(a % 3 == 0):
+    print('fizz')
+elif(a % 5 == 0):
+    print('buzz')
+else:
+    print(a)
