c5aeba8f4d413b41d26165466820e9bae0958460
huifenglan <huifeng1@vt.edu>
2019-05-29 08:49:39 -0400

    Update deliverable.py

--- activities/pe0/part3/deliverable.py
+++ activities/pe0/part3/deliverable.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 def guess_number(n):
+
+    number = int(input("please guess a number between 0 to 100 for fun" + "\n"))
+    while(n != number):
+        if(number < n):
+            print("Too Low")
+            number = int(input("please guess a number between 0 to 100 for fun" + "\n"))
+        elif(number > n):
+            print("Too High")
+            number = int(input("please guess a number between 0 to 100 for fun" + "\n"))
+        '''else:
+           return print("WIN")
+           break'''
+    print("WIN")   
+    return 0
+
     pass
 
 guess_number(23)
