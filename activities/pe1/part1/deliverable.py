9788f8591cf9911fbc32ba6cfb383e5c05c21168
huifenglan <huifeng1@vt.edu>
2019-05-29 10:01:34 -0400

    Update deliverable.py
    finished

--- activities/pe1/part1/deliverable.py
+++ activities/pe1/part1/deliverable.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 
 def invert(l):
+    index = 0
+    for element in l:
+        l[index] = str(255 - int(element))
+        index = index + 1
+    return  
+       
     '''Inverts the given list
     Args:
         l (list): list of strings representing integers in the range [0-255]
@@ -10,6 +16,12 @@ def invert(l):
     pass
 
 def inverted(l):
+    index = 0
+    newlist = l[:]
+    for element in newlist:
+        newlist[index] = str(255 - int(element))
+        index = index + 1
+    return newlist
     '''Returns a new list that is the given list inverted
     Args:
         l (list): list of strings representing integers in the range [0-255]
@@ -19,4 +31,6 @@ def inverted(l):
     pass
 
 if __name__ == '__main__':
+    #a = [1,2,3,4,5]
+    #print(inverted(a))    
     pass
