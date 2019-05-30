ed76fa9b7bd83301131508808a0feef54472110e
huifenglan <huifeng1@vt.edu>
2019-05-30 15:42:50 -0400

    Update deliverable.py

--- activities/pe1/part4/deliverable.py
+++ activities/pe1/part4/deliverable.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
 def read_pgm(filename):
+    with open(filename) as fp:
+        data = fp.read()
+    data = data.split()
+    #header = data[:4]
+    #pixel = data[4:]
+    return(data[:4],data[4:])
+    #print(header)    
+        
+    #return result
+    #return (tuple(header,pixel))
+
+    #print(pixel)
+    #print (data[0:])
+    #print(type(data))
+    #print(data)
     '''Reads a PGM file
     Args:
         filename (str): the file name of a PGM file on disk to read from
@@ -12,6 +27,10 @@ def read_pgm(filename):
     pass
 
 def write_pgm(filename,content):
+    with open(filename,'w') as fp:
+        #for p in content[0]+content[1]
+            #fp.write('{}\n'.format(p))
+        fp.write('\n'.join(content[0]+content[1]))
     '''Writes a PGM file
     Args:
         filename (str): the file name to be used for the written file
@@ -24,6 +43,8 @@ def write_pgm(filename,content):
     pass
 
 def invert(content):
+    for pi in range(len(content[1])):
+        content[1][pi] = str(255 - int(content[1][pi]))
     '''Modifies the pixel intensities of the given content to be inverted
     Args:
         content (tuple):
@@ -35,4 +56,5 @@ def invert(content):
     pass
 
 if __name__ == '__main__':
-	pass
+    #read_pgm('plain.pgm')	
+    pass
